8849488ec718b776a1d267284906f17bbf7c9faf
CAMac <chianhsieh511@gmail.com>
2017-12-18 21:55:50 +0800

    補傳上次漏掉的檔案

---
+++ app/src/main/java/ca/pet/dejavu/Utils/AsyncTask/ImageInsertTask.java
@@ -0,0 +1,104 @@
+package ca.pet.dejavu.Utils.AsyncTask;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.bumptech.glide.Glide;
+
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+import ca.pet.dejavu.Model.IDataModel;
+import ca.pet.dejavu.Presenter.MainPresenter;
+import ca.pet.dejavu.Utils.DBService;
+import ca.pet.dejavu.Utils.MyApplication;
+import ca.pet.dejavu.Utils.SPConst;
+import ca.pet.dejavu.Utils.Table.DataEntity;
+import ca.pet.dejavu.Utils.Table.DataEntityDao;
+
+/**
+ * Created by CAMac on 2017/12/15.
+ * 用於新增圖片的Async Task.
+ */
+
+public class ImageInsertTask extends NormalActionTask {
+
+    private Uri[] uris;
+
+    public ImageInsertTask(MainPresenter presenter, int actionId, @NonNull Uri... uris) {
+        super(presenter, actionId, null, null);
+        this.uris = uris;
+    }
+
+    @Override
+    protected Integer doInBackground(IDataModel... iDataModels) {
+
+        Context context = MyApplication.getContext();
+        DataEntityDao entityDao = DBService.getInstance().getDataEntityDao();
+        int successCount = 0;
+
+        File dir = checkAndMkDir(context);
+        if (null == dir) {
+            return 0;
+        }
+
+        for (Uri uri : uris) {
+            try {
+                Bitmap bitmap = Glide.with(context).load(uri).asBitmap().into(400, 400).get();
+                if (bitmap != null) {
+
+                    DataEntity entity = new DataEntity();
+                    entity.setType(SPConst.VISIBLE_TYPE_IMAGE);
+                    Long id = entityDao.insert(entity);
+
+                    File file = new File(dir, "image_" + id + ".webp");
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    bitmap.compress(Bitmap.CompressFormat.WEBP, 100, baos);
+
+                    BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream(file));
+                    outputStream.write(baos.toByteArray());
+                    outputStream.flush();
+                    outputStream.close();
+                    baos.close();
+                    bitmap.recycle();
+
+                    entity.setTitle(file.getName());
+                    entity.setThumbnailUrl(file.getAbsolutePath());
+                    entityDao.update(entity);
+
+                    successCount++;
+                }
+            } catch (InterruptedException | ExecutionException | IOException e) {
+                e.printStackTrace();
+                Log.e("DEJAVU", "Load Bitmap Error.");
+                Log.e("DEJAVU", "Uri: " + uri.toString());
+            }
+        }
+        return successCount;
+    }
+
+    private File checkAndMkDir(Context context) {
+        File path = new File(context.getFilesDir(), "images");
+        int tryMkDirTimes = 0;
+        while (!path.exists() && !path.mkdir()) {
+            if (tryMkDirTimes > 4) {
+                return null;
+            }
+            tryMkDirTimes++;
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+        return path;
+    }
+}
---
+++ app/src/main/java/ca/pet/dejavu/Utils/AsyncTask/NormalActionTask.java
@@ -0,0 +1,76 @@
+package ca.pet.dejavu.Utils.AsyncTask;
+
+import android.os.AsyncTask;
+
+import java.lang.ref.WeakReference;
+
+import ca.pet.dejavu.Model.DataEntityModel;
+import ca.pet.dejavu.Model.IDataModel;
+import ca.pet.dejavu.Presenter.MainPresenter;
+import ca.pet.dejavu.Utils.Table.DataEntity;
+
+/**
+ * Created by CAMac on 2017/12/15.
+ * 用於連結資料庫操作的async task
+ */
+
+public class NormalActionTask extends AsyncTask<IDataModel, Void, Integer> {
+
+    private WeakReference<MainPresenter> weakPresenter;
+
+    private int actionId;
+    private DataEntity entity;
+    private String title;
+
+    public NormalActionTask(MainPresenter presenter, int actionId, DataEntity entity, String title) {
+        weakPresenter = new WeakReference<>(presenter);
+        this.actionId = actionId;
+        this.entity = entity;
+        this.title = title;
+    }
+
+    /**
+     * 前置作業
+     * 開啟ProgressDialog
+     */
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        MainPresenter presenter = weakPresenter.get();
+        if (presenter != null) {
+            presenter.showProgress();
+        }
+    }
+
+    /**
+     * 進行資料庫操作
+     *
+     * @param iModel interface of DataEntity model
+     * @return position
+     */
+    @Override
+    protected Integer doInBackground(IDataModel... iModel) {
+        int tag = iModel[0].doAction(actionId, entity, title);
+        if (actionId == DataEntityModel.ACTION_INSERT) {
+            tag = 1;
+        }
+        return tag;
+    }
+
+    /**
+     * 後續處理
+     * 調用afterDoAction
+     * 關閉ProgressDialog
+     *
+     * @param position index
+     */
+    @Override
+    protected void onPostExecute(Integer position) {
+        super.onPostExecute(position);
+        MainPresenter presenter = weakPresenter.get();
+        if (presenter != null) {
+            presenter.afterDoAction(actionId, position);
+            presenter.dismissProgress();
+        }
+    }
+}
